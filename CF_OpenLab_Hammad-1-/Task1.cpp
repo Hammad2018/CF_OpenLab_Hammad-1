008fff5d6c649f6d3f6cbcf436bb9f1b3c1072fb
MandarkHammer <132929800+Hammad2018@users.noreply.github.com>
2023-11-02 22:22:26 +0500

    Update Task1.cpp

--- CF_OpenLab_Hammad-1-/Task1.cpp
+++ CF_OpenLab_Hammad-1-/Task1.cpp
@@ -1,3 +1,5 @@
+// Code for Task 1
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,7 +14,7 @@ int main() {
 		cout << "Which book do you want to issue?" << endl;
 		cin >> book_name;
 
-		cout << book_name << " issued." << endl;
+		cout << "Book issued." << endl;
 	}
 
 	else if (operation == "R") {
@@ -33,4 +35,62 @@ int main() {
 	else {
 		cout << "Do you want to issue or return a book (I/R)? " << endl;
 	}
+}
+
+//Code for Task 2:
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    if (num <= 3)
+        return true;
+    if (num % 2 == 0 || num % 3 == 0)
+        return false;
+
+    for (int m = 5; m * m <= num; m += 6) {
+        if (num % m == 0 || num % (m + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void PrimeNuminRange(int s, int e) {
+    cout << "Prime numbers in the range [" << s << ", " << e << "]: ";
+    for (int number = s; number <= e; ++number)
+        if (isPrime(number))
+            cout << number << " ";
+    cout << endl;
+}
+
+int main() {
+    int choice;
+    do {
+        cout << "Choose an option:" << endl;
+        cout << "1. Find prime numbers in a range" << endl;
+        cout << "2. Check if a specific number is prime" << endl;
+        cout << "3. Quit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            int s, e;
+            cout << "Enter the range (start, end): ";
+            cin >> s >> e;
+            PrimeNuminRange(s, e);
+        }
+        else if (choice == 2) {
+            int num;
+            cout << "Enter a number: ";
+            cin >> num;
+            cout << num << (isPrime(num) ? " is prime." : " is not prime.") << endl;
+        }
+        else if (choice != 3)
+            cout << "Invalid choice. Please try again." << endl;
+    } while (choice != 3);
+
+    return 0;
 }
